8f6843896aeb5bd9bf6f6690c8fc921627a789ef
Jufo-Projekt <75041626+Jufo-Projekt@users.noreply.github.com>
2021-10-12 16:39:40 +0100

    Add day/night icon switch

--- weatherstation.ino
+++ weatherstation.ino
@@ -9,24 +9,37 @@ dht11 DHT11;
 SoftTimer dht11timer;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-byte Sus[8] =
-{
-0b11111,
-0b10001,
-0b10001,
-0b11111,
-0b11111,
-0b11111,
-0b11011,
-0b11011
-};
+int edge = 500;
 
+byte Day[8] =
+{
+0b00100,
+0b10001,
+0b01110,
+0b01010,
+0b01110,
+0b10001,
+0b00100,
+0b00000
+};
+byte Night[8] =
+{
+0b11100,
+0b01110,
+0b00111,
+0b00011,
+0b00011,
+0b00111,
+0b01110,
+0b11100
+};
 
 void setup() {
   dht11timer.setTimeOutTime(2000);
   point.attach(6);
   lcd.begin(16, 2);
-  lcd.createChar(0, Sus);
+  lcd.createChar(0, Night);
+  lcd.createChar(1, Day);
   lcd.print("temp: ");
   lcd.setCursor(0,1);
   lcd.print("hum: ");
@@ -40,7 +53,11 @@ void loop() {
   int servoangle = map(lgt, 200, 1000, 0, 180);
   point.write(servoangle);
   lcd.setCursor(15, 0);
-  lcd.write(byte(0));
+  if (lgt > edge) {
+    lcd.write(byte(1));
+  } else {
+    lcd.write(byte(0));
+  }
   lcd.setCursor(10, 1);
   lcd.print(lgt);
   if (dht11timer.hasTimedOut())
